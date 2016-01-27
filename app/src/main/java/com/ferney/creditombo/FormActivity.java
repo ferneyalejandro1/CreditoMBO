d550d6f0c6211516157f92b4998420822966b9db
Ferney <ferneyalejandro1@hotmail.com>
2016-01-27 01:24:20 -0500

    receiver form and 2nd search
    creacion de los campos para capturar los valores del formulario y
    creacion de la segunda pantalla para desplegar la busqueda

--- app/src/main/java/com/ferney/creditombo/FormActivity.java
+++ app/src/main/java/com/ferney/creditombo/FormActivity.java
@@ -4,13 +4,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FormActivity extends AppCompatActivity {
+
+    EditText etNombre, etCedula, etDireccion, etTelefono, etCelular, etOtro, etEmpresa, etValor, etInteres, etCuotas;
+    Button bGuardar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        etNombre = (EditText)findViewById(R.id.nombre);
+        etCedula = (EditText)findViewById(R.id.cedula);
+        etDireccion = (EditText)findViewById(R.id.direccion);
+        etTelefono = (EditText)findViewById(R.id.telefono);
+        etCelular = (EditText)findViewById(R.id.celular);
+        etOtro = (EditText)findViewById(R.id.otro);
+        etEmpresa = (EditText)findViewById(R.id.empresa);
+        etValor = (EditText)findViewById(R.id.valor);
+        etInteres = (EditText)findViewById(R.id.interes);
+        etCuotas = (EditText)findViewById(R.id.cuotas);
+
+        bGuardar = (Button)findViewById(R.id.guardar);
+    }
+
+    public void nuevo_credito(View view){
+        String nombre = etNombre.getText().toString();
+        String cedula = etCedula.getText().toString();
+        String direccion = etDireccion.getText().toString();
+        String telefono = etTelefono.getText().toString();
+        String celular = etCelular.getText().toString();
+        String otro = etOtro.getText().toString();
+        String empresa = etEmpresa.getText().toString();
+        int valor = Integer.parseInt(etValor.getText().toString());
+        int interes = Integer.parseInt(etInteres.getText().toString());
+        int cuotas = Integer.parseInt(etCuotas.getText().toString());
     }
 
     @Override
--- app/src/main/java/com/ferney/creditombo/MenuActivity.java
+++ app/src/main/java/com/ferney/creditombo/MenuActivity.java
@@ -1,26 +1,24 @@
 package com.ferney.creditombo;
 
 import android.annotation.TargetApi;
-import android.app.Activity;
 import android.app.ActivityOptions;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.transition.Explode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.EditText;
 
-public class MenuActivity extends Activity {
+public class MenuActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.ferney.creditombo.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //enable transition
-        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        //getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
         setContentView(R.layout.activity_menu);
     }
 
@@ -32,12 +30,13 @@ public class MenuActivity extends Activity {
     }
 
     @TargetApi(21)
-    public void consulta(View view){
+    public void activity_search(View view){
         getWindow().setExitTransition(new Explode());
-        Intent intent = new Intent(this, ListActivity.class);
+        Intent intent = new Intent(this, SearchActivity.class);
         EditText editText = (EditText) findViewById(R.id.ingrese_nombre);
         String msg = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, msg);
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
 
     @Override
@@ -45,5 +44,19 @@ public class MenuActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
